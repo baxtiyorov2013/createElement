63495a3ee54d35323e16922f429dd77a9cfec205
baxtiyorov2013 <baxtiyorov2013>
2024-04-25 07:21:55 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+// const text = document.createTextNode("Click me...")
+// buttonCharger.appendChild(text);
+// main.appendChild(buttonCharger);
+
+
+
+const h1 = document.createElement("h1");
+let ism = prompt("Imingizni kriting")
+const body = document.querySelector("body")
+let b = body.appendChild(h1)
+const main = document.querySelector("body")
+h1.innerHTML = ism
+
+h1.style.color = 'yellow'
+h1.style.textAlign = 'center'
+h1.style.marginTop = '50vh' 
+h1.style.fontSize = "60px"
+
+main.style.backgroundColor = 'blue'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const function1 = () => {
+//     const subtitle = document.querySelector("h1")
+//     subtitle.textContent = "Hello Mars"
+// }
+// const chench = document.getElementById("btnCharger")
+// chench.onclick = function1
